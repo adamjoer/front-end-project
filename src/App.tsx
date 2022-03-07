6b01605917b2c1f2c878404cedb34d0501eb655a
ak5ina <43174060+ak5ina@users.noreply.github.com>
2022-03-07 16:42:41 +0100

    Added navbar and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Myprofile from './pages/myprofile';
+import Login from './pages/login';
+import Recipes from './pages/recipes';
+import Favorites from './pages/favorites';
+import Lists from './pages/lists';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}></Route>
+          <Route path="myprofile" element={<Myprofile/>}></Route>
+          <Route path="login" element={<Login/>}></Route>
+          <Route path="recipes" element={<Recipes/>}></Route>
+          <Route path="favorites" element={<Favorites/>}></Route>
+          <Route path="lists" element={<Lists/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/navbar.tsx
@@ -0,0 +1,27 @@
+import { AppBar, IconButton, Toolbar } from "@mui/material";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+    return <AppBar position="static">
+        <Toolbar >
+            <IconButton component={Link} to="/" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                Home
+            </IconButton>
+            <IconButton component={Link} to="/myprofile" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                My profile
+            </IconButton>
+            <IconButton component={Link} to="/login" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                Login
+            </IconButton>
+            <IconButton component={Link} to="/recipes" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                Recipies
+            </IconButton>
+            <IconButton component={Link} to="/favorites" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                Favorits
+            </IconButton>
+            <IconButton component={Link} to="/lists" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                Lists
+            </IconButton>
+        </Toolbar>
+    </AppBar>
+}
