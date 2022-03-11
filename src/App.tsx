331440ea243293f00d924a9d4ed053667446f63d
Adam Harald Jørgensen <adamharald440@gmail.com>
2022-03-11 10:13:23 +0100

    Collapse empty body tags and minor formatting changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Myprofile from './pages/myprofile';
 import Login from './pages/login';
@@ -9,20 +9,19 @@ import Favorites from './pages/favorites';
 import Lists from './pages/lists';
 
 function App() {
-  return (<>
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>}></Route>
-          <Route path="myprofile" element={<Myprofile/>}></Route>
-          <Route path="login" element={<Login/>}></Route>
-          <Route path="recipes" element={<Recipes/>}></Route>
-          <Route path="favorites" element={<Favorites/>}></Route>
-          <Route path="lists" element={<Lists/>}></Route>
+          <Route index element={<Home/>}/>
+          <Route path="myprofile" element={<Myprofile/>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="recipes" element={<Recipes/>}/>
+          <Route path="favorites" element={<Favorites/>}/>
+          <Route path="lists" element={<Lists/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
-    </>
   );
 }
 
