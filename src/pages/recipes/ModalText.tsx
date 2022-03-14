6f3a995590495922d212c24d0a60cb23ac4c9871
ak5ina <43174060+ak5ina@users.noreply.github.com>
2022-03-14 16:26:48 +0100

    commit  modal

---
+++ src/pages/recipes/ModalText.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Grid from '@mui/material/Grid';
+
+
+
+export const ModalText = () => {
+  return (
+    <div>
+        <h1 className="title" style={{textAlign:"center"}}> Title</h1>
+        <h2 style={{textAlign:"center"}}> Image </h2>
+      <Grid container spacing={2}>
+        
+        <Grid item style={{borderColor:"#476051", borderStyle:"solid", borderWidth:"1px"}} xs={6}>
+            <div className="ingredients" >
+          <h2 style={{textAlign:"center"}}>Ingredients:</h2>
+          <h4> List with ingredients</h4>
+          </div>
+        </Grid>
+        <Grid item style={{borderColor:"#476051", borderStyle:"solid", borderWidth:"1px"}}  xs={6}>
+            <div className="direction">
+          <h2 style={{textAlign:"center"}}>Direction:</h2>
+          <h4> Step by step</h4>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
