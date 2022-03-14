69e5429f20b921f428a6483c3da592331522fbed
Kristin Aune <kraun@stud.ntnu.no>
2022-03-14 16:10:13 +0100

    Small changes on module

--- src/components/RecipeModal.tsx
+++ src/components/RecipeModal.tsx
@@ -1,131 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export const recipeModal = () => {
-  return (
-    <div>recipeModal</div>
-  )
+export interface reciepeModalProps{
+    setObject: (recipe: Boolean) => void;
+    
 }
 
+export class RecipeModal extends Component <{}, reciepeModalProps>{
 
-/** 
-import React from "react";
-import { makeStyles } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
+    render(){
+        return (
+            <div>recipeModal</div>
+        )
 
-function rand() {
-    return Math.round(Math.random() * 20) - 10;
+    }
+
 }
-function getModalStyle() {
-    const top = 50 + rand();
-    const left = 50 + rand();
-    return {
-        top: `${top}%`,
-        left: `${left}%`,
-        transform: `translate(-${top}%, -${left}%)`,
-    };
-}
-const useStyles = makeStyles((theme?: any) => ({
-    modal: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    paper: {
-        position: 'absolute',
-        width: 450,
-        backgroundColor: theme.palette.background.paper,
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-    },
-}));
-export default function SimpleModal() {
-    const classes = useStyles();
-    const [modalStyle] = React.useState(getModalStyle);
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
-    return (
-        <div>
-            <Button variant="contained" color="primary" onClick={handleOpen}>
-                Open Modal
-            </Button>
-            <Modal
-                aria-labelledby="simple-modal-title"
-                aria-describedby="simple-modal-description"
-                open={open}
-                onClose={handleClose}
-            >
-                <div style={modalStyle} className={classes.paper}>
-                    <h2>Simple React Modal</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi accumsan odio enim, non pharetra est ultrices et.
-                    </p>
-                </div>
-            </Modal>
-        </div>
-    );
-}
-
-
-import React from 'react'
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
-type reciepModalProps = {
-    open: (open: boolean) => void
-    setOpen: (setOpen: boolean) => void
-    handleClose(): void
-}
-
-export const RecipeModal = () => {
-    return(
-        <div>Hello</div>
-    )
-}
-
-export const RecipeModal = (props: reciepModalProps) => {
-    const {setOpen, open} = props
-    const {handleClose} = props
-
-  return (
-    <div>
-        
-        <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-     
-    </div>
-  )
-}
-*/
--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -1,47 +1,41 @@
-import {useState} from 'react'
-//import { RecipeModal } from '../components/RecipeModal';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { ModalText } from '../components/ModalText';
 
-
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 800,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 800,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Recipes() {
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const handleOpen = (): void => setOpenModal(true);
+  const handleClose = (): void => setOpenModal(false);
 
-    const [openModal, setOpenModal] = useState<boolean>(false);
-    const handleOpen = ():void => setOpenModal(true);
-    const handleClose = ():void => setOpenModal(false);
-
-    return <div>
-        <h3> Recipes </h3>
-        
-        <Button onClick={handleOpen}>Open modal</Button>
+  return (
+    <div>
+      <h3> Recipes </h3>
+      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         open={openModal}
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
       >
         <Box sx={style}>
-          <ModalText/>
+          <ModalText />
+          <Button onClick={handleClose}> Close modal</Button>
         </Box>
       </Modal>
-        
-     
-        </div>
+    </div>
+  );
 }
-//<Button onClick={handleOpen} variant="outlined">Open modal</Button>
-//{open && (<RecipeModal  setOpen={setOpen} handleClose={handleClose} open={open}/>)}
