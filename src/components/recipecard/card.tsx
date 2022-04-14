4b7eb22205f7bb6bdf476d166d9e6e3466ac9710
Adam Harald Jørgensen <adamharald440@gmail.com>
2022-04-14 21:22:11 +0200

    Remove unused code and other minor styling changes

--- src/components/recipecard/card.tsx
+++ src/components/recipecard/card.tsx
@@ -1,18 +1,17 @@
-import './card.css';
-import React, { useState } from "react";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import {CardActionArea, Modal, Box, Button} from '@mui/material';
-import SoupKitchenIcon from '@mui/icons-material/SoupKitchen';
-import AvTimerIcon from '@mui/icons-material/AvTimer';
-import StarIcon from '@mui/icons-material/Star';
-import StarHalfIcon from '@mui/icons-material/StarHalf';
-import StarOutlineIcon from '@mui/icons-material/StarOutline';
-import { ModalText } from '../../pages/recipes/ModalText';
+import "./card.css";
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+import SoupKitchenIcon from "@mui/icons-material/SoupKitchen";
+import AvTimerIcon from "@mui/icons-material/AvTimer";
+import StarIcon from "@mui/icons-material/Star";
+import StarHalfIcon from "@mui/icons-material/StarHalf";
+import StarOutlineIcon from "@mui/icons-material/StarOutline";
 
-interface NavbarProps {
+interface ActionAreaCardProps {
   imageString: string;
   titleString: string;
   rank: Number;
@@ -21,46 +20,44 @@ interface NavbarProps {
   selectFunc: any;
 }
 
-const counter = [1, 2, 3, 4, 5]
+const ActionAreaCard = (props: ActionAreaCardProps) => {
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 800,
-  height: '80%',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  overflow: "scroll"
+  const counter = [1, 2, 3, 4, 5]
 
- 
-};
-
-const ActionAreaCard: React.FC<NavbarProps> = (props) => {
-  const [openModal, setOpenModal] = useState<boolean>(false)
-  const handleOpen = (): void => setOpenModal(true)
-  const handleClose = (): void => setOpenModal(false)
-
-  const handleClose_test = ():void => {
-    console.log('testing')
-    setOpenModal(false);
-  };
+  const getStringFromTime = (timeInMin: Number): string => {
+    if (timeInMin <= 8) {
+      return "Around 5 min"
+    } else if (timeInMin <= 12) {
+      return "Around 10 min"
+    } else if (timeInMin <= 18) {
+      return "Around 15 min"
+    } else if (timeInMin <= 25) {
+      return "Around 20 min"
+    } else if (timeInMin <= 37) {
+      return "Around 30 min"
+    } else if (timeInMin <= 50) {
+      return "Around 45 min"
+    } else if (timeInMin <= 70) {
+      return "Around 1 hour"
+    } else if (timeInMin <= 100) {
+      return "Around 1 hour and 30 min"
+    } else if (timeInMin <= 135) {
+      return "Around 2 hours"
+    } else {
+      return "3 hours or more"
+    }
+  }
 
   return (
-    <Card onClick={() => {
-      props.selectFunc();
-    }}>
+    <Card onClick={() => props.selectFunc()}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
           image={props.imageString}
-          alt="green iguana"
+          alt={props.titleString}
         />
-        <CardContent className='cardcontentwrap'>
+        <CardContent className="cardcontentwrap">
           <Typography gutterBottom variant="h5">
             {props.titleString}
           </Typography>
@@ -91,32 +88,6 @@ const ActionAreaCard: React.FC<NavbarProps> = (props) => {
       </CardActionArea>
     </Card>
   );
-}
-
-function getStringFromTime(timeInMin: Number) {
-  if (timeInMin <= 8) {
-    return "Around 5 min"
-  } else if (timeInMin <= 12) {
-    return "Around 10 min"
-  } else if (timeInMin <= 18) {
-    return "Around 15 min"
-  } else if (timeInMin <= 25) {
-    return "Around 20 min"
-  } else if (timeInMin <= 37) {
-    return "Around 30 min"
-  } else if (timeInMin <= 50) {
-    return "Around 45 min"
-  } else if (timeInMin <= 70) {
-    return "Around 1 hour"
-  } else if (timeInMin <= 100) {
-    return "Around 1 hour and 30 min"
-  } else if (timeInMin <= 135) {
-    return "Around 2 hours"
-  } else {
-    return "3 hours or more"
-  }
-
-
 }
 
 export default ActionAreaCard;
