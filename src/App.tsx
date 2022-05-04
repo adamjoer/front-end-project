6d04a40428f1e6efac7846ec1396c9487973f0a9
Adam Harald Jørgensen <adamharald440@gmail.com>
2022-05-04 12:58:55 +0200

    Move Firebase variables to .env file

--- src/App.tsx
+++ src/App.tsx
@@ -21,14 +21,14 @@ import {
 import {getDatabase, ref, set} from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD43KvYWj6dAG_ILb5tkDgXLKXbHxXYuf0",
-  authDomain: "food2side.firebaseapp.com",
-  databaseURL: "https://food2side-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "food2side",
-  storageBucket: "food2side.appspot.com",
-  messagingSenderId: "616953678318",
-  appId: "1:616953678318:web:2a4cad4ed1528458e0a6ff",
-  measurementId: "G-N5ST53G5HR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
